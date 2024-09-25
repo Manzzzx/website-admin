e6b9c2bc1e70445e0de29155717ef6bc16be66b0
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-25 19:34:44 +0700

    Sesi 3 - Final Komponen Pembuatan Toko (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 const SetupPage = () => {
+  
   const onOpen = useStoreModal((state) => state.onOpen)
   const isOpen = useStoreModal((state) => state.isOpen)
 
