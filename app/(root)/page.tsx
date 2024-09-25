0c695e16daa3698a56a757d040299513e5c6dee9
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-25 18:32:05 +0700

    Sesi 3 - Test Dialog Yang Akan Muncul Untuk Membuat Store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/"/>
+      <Modal 
+      title="Test Title" 
+      description="Test Description" 
+      isOpen
+      onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 }
