159ce1c5f99899b19d153b5a4922d5386e9b6b3e
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-30 20:09:29 +0700

    Sesi 5 - Connection database

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,78 +1,71 @@
-'use client'
+"use client";
 
-import * as z from "zod"
-import { useStoreModal } from "@/hooks/use-store-modal"
-import Modal from "../ui/modal"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
-import { Input } from "../ui/input"
-import { Button } from "../ui/button"
+import axios from "axios";
+import * as z from "zod";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import Modal from "../ui/modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 const formSchema = z.object({
-    name: z.string().min(1),
-})
-
-const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // TODO Buat Toko
-    console.log(values);
-    
-}
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
-    const StoreModal = useStoreModal();
+  const [loading, setLoading] = useState(false);
+  const StoreModal = useStoreModal();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-        },
-    })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-    return (
-        <Modal
-        title="Buat Store"
-        description="Tambahkan Store Untuk Membuat produk Dan Kategori"
-        isOpen={StoreModal.isOpen}
-        onClose={StoreModal.onClose}
-        >
-            <div>
-                <div className="space-y-4 py-2 pb-4">
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Name</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                        placeholder="Nama Toko"
-                                        {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                            />
-                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button
-                                variant="outline"
-                                onClick={StoreModal.onClose}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button 
-                                type="submit"
-                                >
-                                    Continue
-                                </Button>
-                            </div>
-                        </form>
-                    </Form>
-                </div>
-            </div>
-        </Modal>
-    )
-}
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      
+      const response = await axios.post("/api/stores", values);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Modal title="Buat Store" description="Tambahkan Store Untuk Membuat produk Dan Kategori" isOpen={StoreModal.isOpen} onClose={StoreModal.onClose}>
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nama Toko" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={StoreModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
